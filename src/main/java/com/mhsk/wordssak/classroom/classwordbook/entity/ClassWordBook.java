70b6e0085cf6f3ad171b96455f4f2259976ac85d
hellmir <hellmir610@gmail..com>
2025-01-15 18:45:57 +0900

    feat(class-wordbook): Add feature that register class word book (#21)

--- src/main/java/com/mhsk/wordssak/classroom/classwordbook/entity/ClassWordBook.java
+++ src/main/java/com/mhsk/wordssak/classroom/classwordbook/entity/ClassWordBook.java
@@ -1,27 +1,43 @@
 package com.mhsk.wordssak.classroom.classwordbook.entity;
 
+import com.mhsk.wordssak.classroom.classwordbook.dto.RegisterClassWordBookRequest;
 import com.mhsk.wordssak.classroom.entity.Classroom;
 import com.mhsk.wordssak.common.entity.BaseEntity;
 import com.mhsk.wordssak.wordbook.entity.WordBook;
 import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.PROTECTED;
 
 @Entity
+@NoArgsConstructor(access = PROTECTED)
 public class ClassWordBook extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+    @ManyToOne
+    @JoinColumn(name = "classroom_id", nullable = false)
+    private Classroom classroom;
 
-  @ManyToOne
-  @JoinColumn(name = "classroom_id", nullable = false)
-  private Classroom classroom;
+    @ManyToOne
+    @JoinColumn(name = "wordbook_id", nullable = false)
+    private WordBook wordBook;
 
-  @ManyToOne
-  @JoinColumn(name = "wordbook_id", nullable = false)
-  private WordBook wordBook;
+    private String reward;
 
-  private String reward;
+    private boolean activeStatus;
 
-  private boolean activeStatus;
+    private ClassWordBook(Classroom classroom, WordBook wordBook, String reward, boolean activeStatus) {
+        this.classroom = classroom;
+        this.wordBook = wordBook;
+        this.reward = reward;
+        this.activeStatus = activeStatus;
+    }
 
+    public static ClassWordBook from(
+            Classroom classroom, WordBook wordBook, RegisterClassWordBookRequest registerClassWordBookRequest
+    ) {
+        return new ClassWordBook(classroom, wordBook, registerClassWordBookRequest.getReward(), true);
+    }
 }
---
+++ src/main/java/com/mhsk/wordssak/classroom/classwordbook/repository/ClassWordBookRepository.java
@@ -0,0 +1,7 @@
+package com.mhsk.wordssak.classroom.classwordbook.repository;
+
+import com.mhsk.wordssak.classroom.classwordbook.entity.ClassWordBook;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClassWordBookRepository extends JpaRepository<ClassWordBook, Long> {
+}
